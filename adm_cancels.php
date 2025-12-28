19cc0668ef4f9bfe90b3f88e9682139444cec283
hyeonnit <lhyeonnit@gmail.com>
2025-12-28 16:24:00 +0900

    adm_payments, cancels, products list form update

--- adm_cancels.php
+++ adm_cancels.php
@@ -10,49 +10,49 @@ include_once("./inc/adm_sidebar.php");
                         <h4 class="card-title" style="margin-bottom: 0;">취소 내역</h4>
                     </div>
                 </div>
-                <div class="table-responsive">
+                <div class="table-responsive pt-3">
                     <table class="table table-striped table-hover table-bordered">
                         <thead class="thead-dark">
                             <tr>
-                                <th class="text-center" style="width:80px;">
+                                <th class="text-center align-content-center" style="width:80px;">
                                     번호
                                 </th>
-                                <th class="text-center" style="width:150px;">
+                                <th class="text-center align-content-center" style="width:200px;">
                                     관리
                                 </th>
-                                <th class="text-center" style="width:150px;">
+                                <th class="text-center align-content-center" style="width:150px;">
                                     주문번호
                                 </th>
-                                <th class="text-center" style="width:150px;">
+                                <th class="text-center align-content-center" style="width:150px;">
                                     주문상태
                                 </th>
-                                <th class="text-center" style="width:450px;">
+                                <th class="text-center align-content-center" style="width:450px;">
                                     상품정보
                                 </th>
-                                <th class="text-center" style="width:150px;">
+                                <th class="text-center align-content-center" style="width:200px;">
                                     구매자 정보
                                 </th>
-                                <th class="text-center" style="width:200px;">
+                                <th class="text-center align-content-center" style="width:200px;">
                                     구매자 주소
                                 </th>
-                                <th class="text-center" style="width:140px;">
-                                    구매일시
+                                <th class="text-center align-content-center" style="width:140px;">
+                                    주문일시
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     번호
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     <input type="button" class="btn btn-outline-primary btn-sm" value="상세" onclick="location.href=''" />
                                     <input type="button" class="btn btn-outline-danger btn-sm" value="삭제" onclick="" />
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     주문번호
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     주문상태
                                 </td>
                                 <td>
@@ -60,23 +60,23 @@ include_once("./inc/adm_sidebar.php");
                                         <img src="" onerror="this.src=''" class="mr-3" alt="">
                                         <div class="mr-3">
                                             <small class="mt-2">상품명</small>
-                                            <span class="text-info">가격</span>
+                                            <p class="text-info">가격</p>
                                         </div>
                                     </div>
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     구매자 이름<br>
                                     구매자 번호
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     구매자 주소
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     주문일시
                                 </td>
                             </tr>
                             <tr>
-                                <td colspan="8" class="text-center"><b>자료가 없습니다.</b></td>
+                                <td colspan="8" class="text-center align-content-center"><b>자료가 없습니다.</b></td>
                             </tr>
                         </tbody>
                     </table>
--- adm_products.php
+++ adm_products.php
@@ -17,32 +17,32 @@ include_once("./inc/adm_sidebar.php");
                     <table class="table table-striped table-hover table-bordered">
                         <thead class="thead-dark">
                             <tr>
-                                <th class="text-center" style="width:80px;">
+                                <th class="text-center align-content-center" style="width:80px;">
                                     번호
                                 </th>
-                                <th class="text-center" style="width:150px;">
+                                <th class="text-center align-content-center" style="width:200px;">
                                     관리
                                 </th>
-                                <th class="text-center" style="width:450px;">
+                                <th class="text-center align-content-center" style="width:450px;">
                                     상품정보
                                 </th>
-                                <th class="text-center" style="width:80px;">
+                                <th class="text-center align-content-center" style="width:80px;">
                                     재고
                                 </th>
-                                <th class="text-center" style="width:100px;">
+                                <th class="text-center align-content-center" style="width:100px;">
                                     판매 상태
                                 </th>
-                                <th class="text-center" style="width:140px;">
+                                <th class="text-center align-content-center" style="width:140px;">
                                     등록일시
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     번호
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     <input type="button" class="btn btn-outline-primary btn-sm" value="수정" onclick="location.href=''" />
                                     <input type="button" class="btn btn-outline-danger btn-sm" value="삭제" onclick="" />
                                 </td>
@@ -56,18 +56,18 @@ include_once("./inc/adm_sidebar.php");
                                         </div>
                                     </div>
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     재고
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     판매상태
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center align-content-center">
                                     등록일시
                                 </td>
                             </tr>
                             <tr>
-                                <td colspan="6" class="text-center"><b>자료가 없습니다.</b></td>
+                                <td colspan="6" class="text-center align-content-center"><b>자료가 없습니다.</b></td>
                             </tr>
                         </tbody>
                     </table>
